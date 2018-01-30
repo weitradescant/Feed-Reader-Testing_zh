5dcd1b3fb22a97b83a4b1a0d182f97dde4d4a905
WeiTradescant <weitradescant@163.com>
2018-01-30 15:08:16 +0800

    完成The menu检测

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,20 +44,28 @@ $(function() {
 
     /* TODO: 写一个叫做 "The menu" 的测试用例 */
     describe('The menu',function() {
+        var $body = $('body');
+        var $icon = $('.icon-list')
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
          it('menu is hidden',function() {
-            expect($('body').hasClass('menu-hidden')).toBe(true);
+            expect($body.hasClass('menu-hidden')).toBe(true);
          })
-         
+
         /* TODO:
         * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
         * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
         * 再次点击的时候是否隐藏。
         */
-
+        it('menu toggle hidden when clicked',function() {
+            $icon.trigger('click');
+            expect($body.hasClass('menu-hidden')).not.toBe(true);
+            $icon.trigger('click');
+            expect($body.hasClass('menu-hidden')).toBe(true);
+         });
+        
 
 
 
